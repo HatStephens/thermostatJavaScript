eebb22b8545293379e293c48a35ce4c1b3fd7f02
Stephen Giles <s.giles@hotmail.co.uk>
2014-10-22 12:17:26 +0100

    Temperature can be increased.

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -3,12 +3,12 @@
 describe('Thermostat', function() {
 
 	var thermostat;
+		
+	beforeEach(function() {
+		thermostat = new Thermostat;
+	});
 
 	describe('by default', function() {
-
-		beforeEach(function() {
-			thermostat = new Thermostat;
-		});
 
 		it('is set to 20 degrees', function() {
 			expect(thermostat.temperature).toEqual(20);
@@ -17,6 +17,27 @@ describe('Thermostat', function() {
 		it('power saving mode(TM) should be on', function() {
 			expect(thermostat.isPowerSaveOn).toBe(true);
 		});
+
+		it('should be able to increase the temperature by 1', function() {
+			thermostat.increaseTemperature();
+			expect(thermostat.temperature).toEqual(21);
+		});
+
+		it('should be able to decrease the temperature by 1', function() {
+			thermostat.decreaseTemperature();
+			expect(thermostat.temperature).toEqual(19);
+		});
+
+	describe('has custom options that', function() {
+
+		it('should be able to increase the temperature by 12', function() {
+			thermostat.increaseTemperatureBy(12);
+			expect(thermostat.temperature).toEqual(32);
+		});
+
+		
+
+	});
 
 	});
 
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -2,3 +2,15 @@ function Thermostat() {
 	this.temperature = 20;
 	this.isPowerSaveOn = true;
 };
+
+Thermostat.prototype.increaseTemperature = function() {
+	this.increaseTemperatureBy(1);
+};
+
+Thermostat.prototype.increaseTemperatureBy = function(degrees) {
+	this.temperature += degrees
+};
+
+Thermostat.prototype.decreaseTemperature = function() {
+	this.temperature -= 1;
+};
